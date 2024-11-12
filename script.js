548723d7941b9353f11b78aa3f3350799e6723c3
naimdevweb <naimhamidi19@gmal.com>
2024-11-12 16:36:24 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,79 @@
+// const alphabetMinuscule = [
+//     "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+//     "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+//   ];
+
+//   let letter = "s";
+//   for (let i = 0; i <= alphabetMinuscule.length;i++){
+//     if(alphabetMinuscule[i] == letter){
+//       console.log(i + 1);
+//     }
+//   };
+
+
+
+  //EXERCICE 5
+//   const nom = "bonjour";
+// const voyelles = ["a","e","i","o","u"];
+
+// console.log("Votre mot a " + nom.length + " lettres")
+
+// console.log("Les voyelles sont : ")
+
+// for (let i = 0 ; i < nom.length ; i++) {
+//     if ( voyelles.includes ( nom.charAt(i) ) )
+//         console.log(nom.charAt(i))
+// }
+
+// nom.forEach ( (lettre) => {if (lettre == "a" | lettre == "e" | lettre == "i" | lettre == "o" | lettre == "u" | lettre == "y") { console.log(lettre)} })
+ 
+
+//EXERCICE 6
+/*
+const phrase = "salut tout le monde";
+let time = 0;
+let variable = "";
+let counted = []; 
+for (let i = 0; i < phrase.length; i++) {
+  if (phrase[i] === " " || counted.includes(phrase[i])) {
+    continue; 
+  }
+
+  variable = phrase[i];
+  time = 0; 
+
+  
+  for (let letter = 0; letter < phrase.length; letter++) {
+    if (phrase[letter] === variable) {
+      time++;
+    }
+  }
+
+  console.log(variable + ": " + time); 
+  counted.push(variable); 
+}
+*/
+   
+  
+
+// function rectangle(longueur , largeur){
+//   if (longueur === largeur){
+//     return ("le resultats est un carre")
+//   }
+//   else{
+//     return ("le resultats est un rectangle")
+//   }
+
+// }
+// console.log(rectangle (5, 8));
+
+//EXERCICE 7
+
+let nombre  = [5,3,8,10,15];
+let positif = [10,8];
+let negatif = [5,3,15];
+for(i= 0; i > nombre.length; i++){
+
+};
+console.log(nombre);
+
