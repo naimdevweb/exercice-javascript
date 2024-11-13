8602f8b4a6307aca17ed844c0971195588b682fd
naimdevweb <naimhamidi19@gmal.com>
2024-11-13 09:08:34 +0100

    1ere partie fini

--- script.js
+++ script.js
@@ -69,11 +69,186 @@ for (let i = 0; i < phrase.length; i++) {
 
 //EXERCICE 7
 
-let nombre  = [5,3,8,10,15];
-let positif = [10,8];
-let negatif = [5,3,15];
-for(i= 0; i > nombre.length; i++){
+/*
+let nombres = [5, -3, 12, -7, 9, -2, 0, -15, 8];
 
-};
-console.log(nombre);
+
+let positifs = [];
+let négatifs = [];
+
+
+for (let i = 0; i < nombres.length; i++) {
+    if (nombres[i] >= 0) {
+        positifs.push(nombres[i]);
+    } else {
+        négatifs.push(nombres[i]);
+    }
+}
+
+
+console.log("Nombres positifs :", positifs);
+console.log("Nombres négatifs :", négatifs);
+*/
+
+//EXERCICE 8
+
+// let nombres = [1,6,8,9,5];
+// for (let i = 0; i < 5; i++) { 
+// }
+
+// let plusGrand = nombres[0];
+
+// for (let i = 1; i < nombres.length; i++) {
+//     if (nombres[i] > plusGrand) {
+//         plusGrand = nombres[i];
+//     }
+// }
+
+
+// console.log("Le plus grand nombre est :", plusGrand);
+
+
+//EXERCICE 9
+
+// let mot = "bonjour"
+// let motInverse = mot.split("").reverse().join("");
+// console.log(motInverse)
+
+
+//PARTIE 2
+
+//EXERCICE 1
+// let texte = "bonjour tout le monde";
+// alert(texte);
+
+//EXERCICE 2
+
+// let lastName = "Dupont";
+// let firstName = "Jean";
+// let city = "Sainte";
+
+// alert("Nom : " + lastName + "\nPrénom : " + firstName + "\nVille : " + city);
+
+//EXERCICE 3
+
+// let prenom = prompt("Saisissez votre prénom :");
+
+// alert("Bonjour, " + prenom);
+
+//EXERCICE4
+
+// document.addEventListener("DOMContentLoaded", function () {
+
+  
+//   const form = document.getElementById("userForm");
+
+  
+//   form.addEventListener("submit", function (event) {
+     
+//       event.preventDefault();
+
+      
+//       const lastName = document.getElementById("lastname").value;
+//       const firstName = document.getElementById("firstname").value;
+//       const city = document.getElementById("city").value;
+
+      
+//       alert("Nom : " + lastName + "\nPrénom : " + firstName + "\nVille : " + city);
+//   });
+
+// });
+
+//EXERCICE 5
+
+// function calculerMultiplication(premierNombre, deuxiemeNombre) {
+//   let partieEntiere, resultat;
+
+  
+//   partieEntiere = Math.floor(premierNombre);
+
+  
+//   resultat = partieEntiere * deuxiemeNombre;
+
+ 
+  
+//   alert("Le résultat de la multiplication est : " + resultat);
+// }
+
+// calculerMultiplication(4.75, 3.2); // Exemple d'appel avec des valeurs arbitraires
+
+ 
+
+//EXERCICE 6
+
+// function afficherReste() {
+  
+
+//   let premierNombre = parseFloat(document.forms['form']['firstNumber'].value); 
+//   let deuxiemeNombre = parseFloat(document.forms['form']['secondNumber'].value);
+
+  
+//   if (isNaN(premierNombre) || isNaN(deuxiemeNombre)) {
+//       alert("Veuillez entrer des nombres valides.");
+//       return; // Arrêter l'exécution si les entrées ne sont pas valides.
+//   }
+
+ 
+//   let reste = premierNombre % deuxiemeNombre;
+
+  
+//   alert("Le reste de la division de " + premierNombre + " par " + deuxiemeNombre + " est : " + reste);
+// }
+
+
+
+//EXERCICE 7
+
+// function calculer(pointure, anneeNaissance) {
+//   let resultat;
+
+  
+//   resultat = (pointure * 2 + 5) * 50 - anneeNaissance + 1766;
+
+ 
+//   alert("Le résultat du calcul est : " + resultat);
+// }
+
+
+// document.getElementById("calculateButton").addEventListener("click", function() {
+  
+//   let pointure = document.querySelector('[name="shoeSize"]').value;
+//   let anneeNaissance = document.querySelector('[name="birthYear"]').value;
+
+  
+//   pointure = parseInt(pointure);
+//   anneeNaissance = parseInt(anneeNaissance);
+
+  
+//   if (isNaN(pointure) || isNaN(anneeNaissance)) {
+//       alert("Veuillez entrer des valeurs valides.");
+//       return;
+//   }
+
+  
+//   calculer(pointure, anneeNaissance);
+// });
+
+//EXERCICE 8
+
+function age(){
+  for(i = 0; i <= 18; i++)
+    if(console.log())
+}
+
+
+
+
+
+
+
+
+
+
+     
+
 
